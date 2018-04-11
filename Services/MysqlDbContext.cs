445b505f781b3f6e140dacd9deb74a0f4c1b045a
jim tang <675040512@qq.com>
2018-04-11 10:28:19 +0800

    rea

--- Services/MysqlDbContext.cs
+++ Services/MysqlDbContext.cs
@@ -1,30 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Services
 {
     public class MysqlDbContext:DbContext
     {
-      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        public MysqlDbContext(DbContextOptions<MysqlDbContext> options)
+            : base(options)
+        { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySQL("server=10.255.19.111;database=mydb;uid=root;pwd=yourpassword;");
+            optionsBuilder.UseMySQL("Server=aspnetcore.cf7plm3xqt5k.us-east-2.rds.amazonaws.com; Port=3306; Database=firstdb; Uid=mysql12; Pwd=T1ang1234; SslMode=Preferred;");
         }
-      public  DbSet<Users> Users{get;set;} 
+
+        public  DbSet<Users> Users{get;set;} 
       public  DbSet<Orders> Orders{get;set;}
     }
+
     [Table("Users")]
     public  class  Users{
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id{get;set;}
          public string UserName{get;set;}
          public string Phone{get;set;} 
-         public visual List<Orders> Orders{get;set;}
+         public virtual List<Orders> Orders{get;set;}
     } 
 
     [Table("Orders")]
     public class Orders{
-         public int Id {get;set;}
-          public visual Users User{get;set;}
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        public int Id {get;set;}
+          public virtual Users User{get;set;}
           
+        [ForeignKey("FK_Orders_Users_UserId")]
           public int  UserId{get;set;}
     }
 }
--- webapp/Controllers/HomeController.cs
+++ webapp/Controllers/HomeController.cs
@@ -4,18 +4,48 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Services;
 using webapp.Models;
 
 namespace webapp.Controllers
 {
+
+  
     public class HomeController : Controller
     {
+        private readonly MysqlDbContext _context;
+
+        public HomeController(MysqlDbContext context)
+        {
+            _context = context;
+        } 
+
         public IActionResult Index()
         {
             ViewData["username"]="welcome jimtang";
+             var users=  _context.Users.ToList().Select(fg=>new UserViewModel() { UserName = fg.UserName, Phone = fg.Phone }).ToList();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "New";
             return View();
         }
 
+        [HttpPost]
+        public  IActionResult Create(UserViewModel user)
+        {
+            var userd = _context.Users.Add(new Users()
+            {
+                Phone = user.Phone,
+                UserName = user.UserName
+
+            });
+            _context.SaveChanges();
+            return Redirect("index");
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
